ff3c35e086f2157366f1bf496a1215664d599d10
GRIEGMAK <g.m.ak@yandex.ru>
2021-06-24 18:00:15 +0600

    Добавил метод который добавляет тестовые данные и закомментировал его

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,4 +18,29 @@ class ProjectsController < ApplicationController
     end
     @todo.save()
   end
+
+  # def creater_test # Раскомментировать для создания списка задач
+  #   @project = Project.create({title: 'семья'})
+  #   @project.todos.create({text: 'Купить молоко', isCompleted: false})
+  #   @project.todos.create({text: 'Заменить масло в двигателе до 23 апреля', isCompleted: false})
+  #   @project.todos.create({text: 'Отправить письмо бабушке', isCompleted: true})
+  #   @project.todos.create({text: 'Заплатить за квартиру', isCompleted: false})
+  #   @project.todos.create({text: 'Забрать обувь из ремонта', isCompleted: false})
+  #   @project.save()
+
+  #   @project = Project.create({title: 'Работа'})
+  #   @project.todos.create({text: 'Позвонить заказчику', isCompleted: true})
+  #   @project.todos.create({text: 'Отправить документы', isCompleted: true})
+  #   @project.todos.create({text: 'Заполнить отчет', isCompleted: false})
+  #   @project.save()
+
+  #   @project = Project.create({title: 'Прочее'})
+  #   @project.todos.create({text: 'Позвонить другу', isCompleted: false})
+  #   @project.todos.create({text: 'Подготовиться к поездке', isCompleted: false})
+  #   @project.save()
+  #   @project = Project.all
+  #   render json: {projects: @project}
+  # end
+
+
 end
